749cffd18f337414b01a194006499c495ceee5bc
Prasad CH <116002750+Prasad-C-H@users.noreply.github.com>
2024-04-29 20:24:28 +0530

    Add files via upload

---
+++ Task1-LandingPage #codSoft/script.js
@@ -0,0 +1,8 @@
+// Hamburger menu toggle
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
